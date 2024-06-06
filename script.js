e284b982c643abd80ff63fea2fbb9f0ddbde6c3e
Kismat Ali <61203535+kismat5455@users.noreply.github.com>
2024-06-06 10:59:40 -0500

    refactor: Add dropdown functionality to improve user experience

--- script.js
+++ script.js
@@ -22,5 +22,22 @@ document.addEventListener("DOMContentLoaded", function() {
     typeWriter();
 });
 
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
 
 
