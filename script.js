237aea010ca8093dade48a6475a7486886495590
Kismat Ali <61203535+kismat5455@users.noreply.github.com>
2024-06-06 12:23:51 -0500

    Improve touch event handling for hover effect on elements

--- script.js
+++ script.js
@@ -21,21 +21,3 @@ document.addEventListener("DOMContentLoaded", function() {
 
     typeWriter();
 });
-
-const elements = document.querySelectorAll('.hover-effect');
-
-elements.forEach((element) => {
-  // Add touchstart event listener
-  element.addEventListener('touchstart', () => {
-    element.classList.add('touch-hover-effect');
-  });
-
-  element.addEventListener('touchend', () => {
-    // Remove touch effect
-    element.classList.remove('touch-hover-effect');
-  });
-});
-
-
-
-
