489b54d78d7b3e3a01540b27a3559b4c6ee3a002
Kismat Ali <61203535+kismat5455@users.noreply.github.com>
2024-06-07 21:26:16 -0500

    Add .gitignore file and update npm dependencies

---
+++ github-proxy/server.js
@@ -0,0 +1,39 @@
+
+
+require('dotenv').config({path:'/home/ubuntu/CSWDP_SUMMER_2024/PersonalPortfolio/.env'});
+
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/repos', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.github.com/users/kismat5455/repos', {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching repositories' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/projects.js
@@ -0,0 +1,42 @@
+const repoList = document.getElementById('repo-list');
+
+// Function to fetch and display repository data with a delay
+const fetchRepoData = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/repos');
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const reposData = await response.json();
+   
+    for (let i = 0; i < reposData.length; i++) {
+      const repo = reposData[i];
+
+      await new Promise(resolve => setTimeout(resolve, 120)); 
+
+      const { name, description, html_url, stargazers_count, forks_count } = repo;
+
+      repoList.innerHTML += `
+        <div class="bg-white rounded-lg shadow-md p-6 m-4 transition duration-300 hover:scale-105">
+          <h3 class="text-xl font-semibold mb-2">${name}</h3>
+          <p class="description text-gray-600">${description || "No description available."}</p>
+          <p class="mt-2 text-sm text-gray-500">
+            Stars: <span class="font-medium text-yellow-500">${stargazers_count}</span> |
+            Forks: <span class="font-medium text-green-500">${forks_count}</span>
+          </p>
+          <a class="github-link mt-4 inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            href="${html_url}" target="_blank">
+            View on GitHub
+          </a>
+        </div>
+      `;
+    }
+  } catch (error) {
+    console.error('Error fetching repository data:', error);
+    repoList.innerHTML += '<div>Error fetching repository data.</div>';
+  }
+};
+
+fetchRepoData();
