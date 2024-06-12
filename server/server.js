68a0b0320954c376ee6d9fd7d8dab1a43ea42d66
Kismat Ali <61203535+kismat5455@users.noreply.github.com>
2024-06-12 11:40:35 -0500

    Update server.js to use environment variable for port number

--- server/server.js
+++ server/server.js
@@ -1,40 +1,39 @@
-require('dotenv').config({ path: '/home/ubuntu/CSWDP_SUMMER_2024/PersonalPortfolio/.env' });
+require("dotenv").config({
+  path: "/home/ubuntu/CSWDP_SUMMER_2024/PersonalPortfolio/.env",
+});
 
-const express = require('express');
-const cors=require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 app.use(cors());
 
-app.use(express.static('../docs'));
+app.use(express.static("../docs"));
 
-app.get('/repos', (req, res) => {
-  fetch('https://api.github.com/users/kismat5455/repos', {
+app.get("/repos", (req, res) => {
+  fetch("https://api.github.com/users/kismat5455/repos", {
     headers: {
-      Authorization: `token ${GITHUB_TOKEN}`
-    }
+      Authorization: `token ${GITHUB_TOKEN}`,
+    },
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    
-    }
-    return response.json();
-  })
-  .then(data => {
-    res.json(data);
-  })
-  .catch(error => {
-    throw new Error('Error fetching repositories');
-  });
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      throw new Error("Error fetching repositories");
+    });
 });
 
-
-app.get('/')
-
+app.get("/");
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
