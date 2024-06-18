9f8f9a16f6211c8a364177f6b4216b7d625b2d41
Kismat Ali <61203535+kismat5455@users.noreply.github.com>
2024-06-17 23:41:53 -0500

    Update server.js to use correct static file path and port number

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
